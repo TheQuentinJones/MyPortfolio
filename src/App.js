592479adae89766ce48974c74c6905d4da5626df
NEO <qj82189@gmail.com>
2023-03-06 21:35:43 -0500

    Trying to make it work

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+
+    <div>
+        <Header />
+        <PortfolioContainer />
+        <Footer />
+    </div>
+};
+
+export default App;
