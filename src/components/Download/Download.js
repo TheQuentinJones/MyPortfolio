b4d4a4d4efc4f16f76e38ec760381aec35accdf8
NEO <qj82189@gmail.com>
2023-03-18 12:10:49 -0400

    adding style to resume button

--- src/components/Download/Download.js
+++ src/components/Download/Download.js
@@ -1,12 +1,12 @@
 const Download = () => {
 
-    const resumeFile = 'https://drive.google.com/file/d/1VjHG4amcDAaT9eYb3Follzn5i05Nda2F/view?usp=sharing'
-
-
+   
     return (
 
         <div>
-           <a href={{resumeFile}} > <button>View My Resume</button> </a>
+           <a href= 'https://drive.google.com/file/d/1VjHG4amcDAaT9eYb3Follzn5i05Nda2F/view?usp=sharing' > 
+                <button className='button is-link is-medium' style={{margin: '5rem', }}>View My Resume</button> 
+           </a>
         </div>
 
     )
