e078844ea23fa57fde6a7f2d2ceb6d9a36c8ca69
NEO <qj82189@gmail.com>
2023-04-26 16:27:49 -0400

    finished the main portion of the portfolio

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Contact.css'
+import { AiTwotoneMail } from 'react-icons/ai'
+import { RiWhatsappFill } from 'react-icons/ri'
 
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">  
+          <article className="contact__option">
+            <AiTwotoneMail className='contact__option-icon' />
+            <h4>Email</h4>
+            <a href="mailto:quentin.mcc.jones@gmail.com" target='_blank'>Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <RiWhatsappFill className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <a href="https://api.whatsapp.com/send?phone=12409181791" target='_blank'>Send a Message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="text" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
     </section>
     
   )
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Footer.css'
+import Socials from '../Socials/Socials'
+import { GoMarkGithub } from 'react-icons/go';
+import { SiLinkedin } from 'react-icons/si'
+import { AiFillInstagram } from 'react-icons/ai'
+import { CgTwitter } from 'react-icons/cg'
+
+const Footer = () => {
+  return (
+    <footer>
+        <a href="#home" className='footer__logo'>Q Jones</a>
+
+        <ul className="permalinks">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer__socials">
+            <a href='https://www.github.com/TheQuentinJones'> 
+                <GoMarkGithub size={28} />
+            </a>
+            <a href='https://www.linkedin.com/in/therealtorque/'>
+                <SiLinkedin size={28} />
+            </a>
+            <a href='https://www.instagram.com/realtor.que/'>
+                <AiFillInstagram size={28} />
+            </a>
+            <a href='https://twitter.com/QueTheRealtor'>
+                <CgTwitter size={28} />
+            </a>   
+            
+        </div>
+
+        <div className="footer__copyright">
+            <small>&copy; Q. Jones. All rights reserved</small>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
