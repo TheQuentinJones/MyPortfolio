af855ae20ad29477369512f1d2d5044a0c9f49a4
NEO <qj82189@gmail.com>
2023-04-26 14:45:52 -0400

    finished portfolio setup

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,72 +1,60 @@
 import React from 'react';
 import './Portfolio.css'
+import  IMG1  from '../../Assets/Screenshot-WorkHive.jpg'
+import  IMG2  from '../../Assets/Fitness4U-screenshot.jpg'
+import  IMG3  from '../../Assets/Screenshot-MovingParts.jpg'
+
+const portfolioData = [
+
+  {
+    id: 1,
+    image: IMG1,
+    title: 'WorkHive',
+    github: 'https://github.com/TrentLe/WorkHive',
+    livesite: 'https://workhive1.herokuapp.com'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Fitness4U',
+    github: 'https://github.com/Henrich77/Fitness4u',
+    livesite: 'https://fitness-4u.herokuapp.com/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'MovingParts',
+    github: 'https://github.com/boy-son/Moving-Parts',
+    livesite: 'https://boy-son.github.io/Moving-Parts/'
+  }
+
+]
+
+
 
 export default function Portfolio() {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
+      <h5>My Work</h5>
+      <h2>Portfolio</h2>
 
-      <h1 className='has-text-centered is-size-2'>Projects</h1>
-      <div className='is-flex is-flex-direction-row'>
-        <div className='cardholder' style={{ height: '30rem', width: '20rem', marginTop: '6rem', marginLeft: '6rem'}}>
-          <a href='https://workhive1.herokuapp.com'>
-            <div className="card" style={{ height: '20rem', width: '20rem'}}>
-              <div className="card-image">
-                <figure className="image is-6by5">
-                  <img style={{ height: '15rem', width: 'auto'}}src="https://i.postimg.cc/nLY8ZVDq/Screenshot-Work-Hive.jpg" alt="Placeholder" />
-                </figure>
+      <div className="container portfolio__container">
+        {
+          portfolioData.map(({ id, image, title, github, livesite}) => {
+            return (
+              <article key={ id } className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={ image } alt={ title } />
               </div>
-              <div className="card-content" style={{}}>
-                <div className="media">
-
-                  <div className="media-content">
-                    <p className="title is-6">WorkHive</p>
-                    <p className="subtitle is-7">Professional Social Networking App</p>            
-                  </div>
-                </div>
+              <h3>{ title }</h3>
+              <div className="portfolio__item-cta">
+                <a href={ github } className='btn' target='_blank'>Github</a>
+                <a href={ livesite } className='btn btn-primary' target='_blank'>Live Website</a>
               </div>
-            </div>
-          </a>
-        </div>
-        <div className='cardholder' style={{ height: '30rem', width: '20rem', marginTop: '6rem', marginLeft: '6rem'}}>
-          <a href='https://fitness-4u.herokuapp.com'>
-            <div className="card" style={{ height: '20rem', width: '20rem'}}>
-              <div className="card-image">
-                <figure className="image is-6by5">
-                  <img style={{ height: '15rem', width: 'auto'}}src="https://i.postimg.cc/sxqr0nkX/Fitness4-U-screenshot.jpg" alt="Placeholder" />
-                </figure>
-              </div>
-              <div className="card-content" style={{}}>
-                <div className="media">
-
-                  <div className="media-content">
-                    <p className="title is-6">Fitness4U</p>
-                    <p className="subtitle is-7">Fitness Workout Tracker App</p>            
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className='cardholder' style={{ height: '30rem', width: '20rem', marginTop: '6rem', marginLeft: '6rem'}}>
-          <a href='https://boy-son.github.io/Moving-Parts/'>
-            <div className="card" style={{ height: '20rem', width: '20rem'}}>
-              <div className="card-image">
-                <figure className="image is-6by5">
-                  <img style={{ height: '15rem', width: 'auto'}}src="https://i.postimg.cc/SxrymG8Z/Screenshot-Moving-Parts.jpg" alt="Placeholder" />
-                </figure>
-              </div>
-              <div className="card-content" style={{}}>
-                <div className="media">
-
-                  <div className="media-content">
-                    <p className="title is-6">MovingParts</p>
-                    <p className="subtitle is-7">HomeSale and Job Search App</p>            
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   );
