bf63cf2f8fdc565f148a1d28ecf35ec5ec9e603b
NEO <qj82189@gmail.com>
2023-04-26 12:28:52 -0400

    finished experience portion

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,9 +1,135 @@
 import React from 'react'
 import './Experience.css'
+import { FiCheckCircle } from 'react-icons/fi'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>The Skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>React.js</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Handlebars.js</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>JQuery</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Bulma</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+{/* End of Frontend */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Node.js</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>MVC</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FiCheckCircle className='experience__details-icon'/>
+              <div>
+                <h4>GraphQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+
+        </div>
+
+      </div>
+      </section>
   )
 }
 
