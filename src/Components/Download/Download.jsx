daf9ff73c3025b24499697f4f9bfd23bfa55f614
NEO <qj82189@gmail.com>
2023-09-30 13:31:06 -0400

    changed resume link

--- src/Components/Download/Download.jsx
+++ src/Components/Download/Download.jsx
@@ -8,7 +8,7 @@ const Download = () => {
     return (
 
         <div className='cta'>
-           <a className='btn' href= 'https://drive.google.com/file/d/1KQnBK_tB3ROmsMestIZN--gDBILAyrHx/view?usp=drive_link' >View My Resume</a>
+           <a className='btn' href= 'https://drive.google.com/file/d/1_Pp96dsWtpXlRUUHIhStHWPV3vLwmHD9/view?usp=drive_link' >View My Resume</a>
            <a className='btn btn-primary' href="#contact">Let's connect!</a>
         </div>
 
